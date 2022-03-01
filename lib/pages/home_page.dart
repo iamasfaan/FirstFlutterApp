ac753d7bb952ac44e7d41e1f31c06772a8c770f9
iamasfaan <iamasfaan@gmai;l.com>
2022-03-01 20:32:52 +0530

    Dummy list created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_catalog/models/catalog.dart';
+import 'package:flutter_project_catalog/widgets/item_widget.dart';
 
 import '../widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
+  final dummyList = List.generate(20, (index) => CatalogModle.Items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Asfaan's App"),
       ),
-      body: Center(
-        child: Text("First App demo"),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(
+            item: dummyList[index],
+          );
+        },
       ),
       drawer: MyDrawer(),
     );
